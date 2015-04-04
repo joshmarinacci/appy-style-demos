f6b910498999f2c4b60311329f9b0ab3cf332545
Josh Marinacci <joshua@marinacci.org>
2015-04-04 13:36:09 -0700

    initial work on keyboard focus and nav for table and table rows

--- musicplayer/ui.jsx
+++ musicplayer/ui.jsx
@@ -29,7 +29,7 @@ var SongDatabase = {
             THRUST.remote.send({type:'running'});
         } else {
             for (var i = 0; i < 100; i++) {
-                this.songs.push({title: 'foo bar baz mister foo bar baz and stuff', artist: 'bar', album: 'baz'});
+                this.songs.push({title: i+' foo bar baz mister foo bar baz and stuff', artist: 'bar', album: 'baz'});
             }
         }
     },
@@ -68,6 +68,13 @@ var SongDatabase = {
     },
     getStatus: function() {
         return this.status;
+    },
+    navNextSong: function(song) {
+        var n = this.songs.indexOf(song);
+        n++;
+        var song = this.songs[n];
+        this.selected = song;
+        this.notify();
     }
 };
 
@@ -76,22 +83,44 @@ SongDatabase.init();
 var SongTableRow = React.createClass({
     clicked: function(e) {
         e.preventDefault();
-        SongDatabase.setSelected(this.props.song);
+        this.props.setSelected(this.props.index);
+        //SongDatabase.setSelected(this.props.song);
+        this.refs.row.getDOMNode().focus();
     },
     doubleClicked: function(e) {
         e.preventDefault();
         SongDatabase.playSong(this.props.song);
     },
+    keypress: function(e) {
+        //console.log("key pressed",e.key);
+        //e.preventDefault();
+        //e.stopPropagation();
+    },
+    keydown: function(e) {
+        //console.log("keydown",e.key);
+        if(e.key == 'ArrowDown') {
+            //console.log('st = ',this.refs.row.getDOMNode().scrollTop);
+            //console.log('off = ',this.refs.row.getDOMNode().offsetTop);
+            //this.props.setSelected(this.props.index+1);
+            //e.preventDefault();
+            //SongDatabase.navNextSong(this.props.song);
+            //return;
+        }
+
+    },
     render: function() {
         var song = this.props.song;
-        var selected = (SongDatabase.getSelected() == song);
+        var selected = this.props.index == this.props.selectedIndex;
         var cn = "";
         if(selected) {
             cn = "selected";
         }
-        return <tr className={cn}
+        return <tr ref='row' tabIndex="1"
+                   className={cn}
                    onClick={this.clicked}
                    onDoubleClick={this.doubleClicked}
+                   onKeyPress={this.keypress}
+                   onKeyDown={this.keydown}
             >
             <td>{song.title}</td>
             <td>00:00</td>
@@ -106,7 +135,8 @@ var ScrollTable = React.createClass({
     getInitialState: function() {
         var songs = SongDatabase.getSongs();
         return {
-            songs:songs
+            songs:songs,
+            selectedIndex:0
         }
     },
     componentDidMount: function() {
@@ -117,13 +147,62 @@ var ScrollTable = React.createClass({
             })
         });
     },
+    keypress: function(e) {
+        console.log("got a key event");
+    },
+    keydown: function(e) {
+        console.log('table got key down',e.key);
+        if(e.key == 'ArrowDown') {
+            this.setSelected(this.state.selectedIndex+1);
+            e.stopPropagation();
+            e.preventDefault();
+            return;
+        }
+        if(e.key == 'ArrowUp') {
+            this.setSelected(this.state.selectedIndex-1);
+            e.stopPropagation();
+            e.preventDefault();
+            return;
+        }
+        if(e.key == 'Enter') {
+            console.log("playing the current song");
+            e.stopPropagation();
+            e.preventDefault();
+            return;
+        }
+    },
+    setSelected: function(n) {
+        if(n < 0) n = 0;
+        if(n >= this.state.songs.length) n = this.state.songs.length-1;
+        this.setState({
+            selectedIndex:n
+        });
+
+        var node = this.refs.body.getDOMNode();
+        var st = this.refs.body.getDOMNode().scrollTop;
+        var h = this.refs.body.getDOMNode().offsetHeight;
+        var off = n * 34;
+        if(off < node.scrollTop) {
+            node.scrollTop = off;
+        }
+        if(off > h+node.scrollTop) {
+            node.scrollTop = off-h;
+        }
+    },
     render: function() {
-        var rows = this.state.songs.map(function(song) {
-            return <SongTableRow song={song} key={song.uid}/>;
+        var self = this;
+        var rows = this.state.songs.map(function(song,i) {
+            return <SongTableRow
+                    song={song}
+                    key={song.uid}
+                    index={i}
+                    selectedIndex={self.state.selectedIndex}
+                    setSelected={self.setSelected}
+                />;
         });
         return (
             <div id="wrapper">
-                <table>
+                <table tabIndex="0">
                     <thead>
                     <th>Name</th>
                     <th>Time</th>
@@ -132,8 +211,12 @@ var ScrollTable = React.createClass({
                     <th>Genre</th>
                     </thead>
                 </table>
-                <div id="body">
-                    <table>
+                <div id="body" ref='body'>
+                    <table
+                        ref="tbody"
+                        onKeyPress={this.keypress}
+                        onKeyDown={this.keydown}
+                        >
                         <tbody>
                         {rows}
                         </tbody>
@@ -156,13 +239,14 @@ var MusicDisplay = React.createClass({
     componentDidMount: function() {
         var self = this;
         SongDatabase.onChange('player',function() {
+            /*
             console.log("new status = ", SongDatabase.getStatus());
             var status = SongDatabase.getStatus();
             self.setState({
                 title: status.song.title,
                 artist: status.song.artist[0],
                 album: status.song.album
-            })
+            })*/
         })
     },
     render: function() {
